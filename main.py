252baa8c3c7c42149b7e3c4cb3ec34792039b3c3
sakkke <84666033+sakkke@users.noreply.github.com>
2024-06-22 08:06:04 +0900

    feat: implement chat command

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from os import getenv
 from discord import ApplicationContext, Bot
+from openai import OpenAI
 
 load_dotenv()
 
 bot = Bot()
+ai = OpenAI()
 
 
 @bot.event
@@ -12,9 +14,24 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
-@bot.slash_command(name="hello", description="Say hello to the bot")
-async def hello(ctx: ApplicationContext):
-    await ctx.respond("Hey!")
+@bot.slash_command(name="chat", description="Chat with GPT-4o")
+async def chat(ctx: ApplicationContext, prompt: str):
+    completion = ai.chat.completions.create(
+        model="gpt-4o",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a assistant.",
+            },
+            {
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+    )
+    message = completion.choices[0].message
+    content = message.content
+    await ctx.respond(content)
 
 
 token = getenv("TOKEN")
